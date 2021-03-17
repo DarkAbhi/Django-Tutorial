7297d91f9e36faa641c21908e55cbdb57c475ee9
Abhishek AN <darkabhi1520@gmail.com>
2021-03-17 14:28:08 +0530

    Django Content Blocks and Snippets
    Signed-off-by: Abhishek AN <darkabhi1520@gmail.com>

--- personal/views.py
+++ personal/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def home_screen_view(request):
     print(request.headers)
-    return render(request, "base.html", {})
+    return render(request, "personal/home.html", {})
