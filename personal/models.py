20f9f64f045f7e5ba9599aef6556386022e313e6
Abhishek AN <darkabhi1520@gmail.com>
2021-05-23 04:53:42 +0530

    Customizing admin panel
    Signed-off-by: Abhishek AN <darkabhi1520@gmail.com>

--- personal/models.py
+++ personal/models.py
@@ -1,21 +1,21 @@
-from django.db import models
+# from django.db import models
 
-# Create your models here.
+# # Create your models here.
 
-PRIORITY = [
-    ("H","High"),
-    ("M","Medium"),
-    ("L","Low")
-]
+# PRIORITY = [
+#     ("H","High"),
+#     ("M","Medium"),
+#     ("L","Low")
+# ]
 
-class Question(models.Model):
-    title                   = models.CharField(max_length=60)
-    question                = models.TextField(max_length=400)
-    priority                = models.CharField(choices = PRIORITY, max_length=1)
+# class Question(models.Model):
+#     title                   = models.CharField(max_length=60)
+#     question                = models.TextField(max_length=400)
+#     priority                = models.CharField(choices = PRIORITY, max_length=1)
 
-    def __str__(self):
-        return self.title
+#     def __str__(self):
+#         return self.title
 
-    class Meta:
-        verbose_name="The Question"
-        verbose_name_plural="Peoples Questions"
+#     class Meta:
+#         verbose_name="The Question"
+#         verbose_name_plural="Peoples Questions"
--- personal/views.py
+++ personal/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from personal.models import Question
+from account.models import Account
 
 # Create your views here.
 
@@ -8,7 +8,7 @@ def home_screen_view(request):
 
     context = {}
 
-    questions = Question.objects.all()
-    context['questions'] = questions
+    accounts = Account.objects.all()
+    context['accounts'] = accounts
 
     return render(request, "personal/home.html", context)
